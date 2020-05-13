3b948080f9e8697b0c1c3c0e90e0be06fe9fe879
sung-oh-park <sung-oh.park@navercorp.com>
2020-05-13 20:57:17 +0900

    initialize Spring Boot project

---
+++ src/test/java/com/blogish/blogish/BlogishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogish.blogish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
