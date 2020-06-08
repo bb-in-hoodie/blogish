992362379abf74199e094bf16e3690127cd8defa
sung-oh-park <sung-oh.park@navercorp.com>
2020-06-08 12:16:18 +0900

    apply bcrypt encoding on password
    - implement SecurityConfig
    - apply bcrypt
    - stop using simpleJdbcInsert as a bean

--- src/main/java/com/blogish/blogish/controller/UserController.java
+++ src/main/java/com/blogish/blogish/controller/UserController.java
@@ -3,6 +3,7 @@ package com.blogish.blogish.controller;
 import com.blogish.blogish.dto.User;
 import com.blogish.blogish.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +15,12 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    BCryptPasswordEncoder bcryptEncoder;
+
     @PostMapping
     public String join(@RequestBody User user) {
+        user.setPassword(bcryptEncoder.encode(user.getPassword()));
         return userRepository.addUser(user);
     }
 }
