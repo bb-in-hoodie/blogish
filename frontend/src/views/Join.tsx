6a5b8bd271971331490199e8e0dd62a78e9d3448
sung-oh-park <sung-oh.park@navercorp.com>
2020-07-25 15:15:09 +0900

    feat: basic FE setup
    - add Reactstrap and Bootstrap dependencies
    - apply NotoSans CJK KR font
    - basic layout
    - apply state on each input

--- frontend/src/views/Join.tsx
+++ frontend/src/views/Join.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+  Form, FormGroup, Label, Input, Button,
+} from 'reactstrap';
+import '../css/join.css';
 
 export default function Join() {
+  // user info
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+
   return (
-    <h1>Join</h1>
+    <div className="join">
+      <div className="header">
+        JOIN
+      </div>
+      <Form>
+        <FormGroup>
+          <Label for="input_id">ID</Label>
+          <div className="input_with_button">
+            <Input id="input_id" type="text" value={id} onChange={(e) => setId(e.target.value)} />
+            <Button color="warning">check</Button>
+          </div>
+        </FormGroup>
+        <FormGroup>
+          <Label for="input_password">Password</Label>
+          <Input id="input_password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </FormGroup>
+        <FormGroup>
+          <Label for="input_name">Name</Label>
+          <Input id="input_name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </FormGroup>
+        <div className="submit_btn_area">
+          <Button color="secondary">Cancel</Button>
+          <Button color="success" className="join_btn">Join</Button>
+        </div>
+      </Form>
+    </div>
   );
 }
