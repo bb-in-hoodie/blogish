e740e0c438f116540a8fbc3fb7bb5efe18bd6ba5
BlackBindy <pso999@naver.com>
2020-11-18 22:21:55 +0900

    feat: blog - display user and blog info
    - display user info on top
    - implement and use blogInfoAPI function
    - display blog info

--- frontend/src/views/BlogView.tsx
+++ frontend/src/views/BlogView.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { Badge } from 'reactstrap';
+import { blogInfoAPI } from '../api/BlogAPI';
 import '../css/blog.css';
+import useUser from '../hooks/useUser';
+import Blog from '../types/Blog';
 
 export default function BlogView(): JSX.Element {
+  const user = useUser(true);
+  const history = useHistory();
   const { blogId } = useParams() as { blogId: string };
+  const [blog, setBlog] = useState<Blog | null>(null);
+
+  useEffect(() => {
+    if (!blogId) {
+      history.push('/browse');
+    }
+
+    async function updateBlog() {
+      try {
+        const blogInfo = await blogInfoAPI(parseInt(blogId, 10));
+        setBlog(blogInfo);
+      } catch (e) {
+        alert('블로그를 불러오는데 실패했습니다.');
+        history.push('/browse');
+      }
+    }
+    updateBlog();
+  }, [blogId]);
 
   return (
     <div className="blog">
       <header className="main_header">
         <div className="user_info">
-          user name here
+          { user.nickname }
         </div>
         <div className="blog_info">
-          <h2>Blog Title Here</h2>
-          <h5>{ `blog description here ${blogId}` }</h5>
+          <h2>{ blog?.title }</h2>
+          {blog?.description && <h5>{ blog?.description }</h5>}
         </div>
       </header>
       <nav>
