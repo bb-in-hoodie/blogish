1d88ded879879ebcc93f3da5566aa9aa215a82a0
BlackBindy <pso999@naver.com>
2020-12-26 16:11:58 +0900

    feat: Write | basic layout and events on title and context

--- frontend/src/components/blog/Write.tsx
+++ frontend/src/components/blog/Write.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Input, Label } from 'reactstrap';
 import Blog, { WriteMode } from '../../types/Blog';
 import Category from '../../types/Category';
 import Post from '../../types/Post';
@@ -15,11 +16,20 @@ interface WriteProps {
 export default function Write({
   mode, blog, categories, activeCategoryId, selectedPost,
 }: WriteProps): JSX.Element {
+  const [title, setTitle] = useState('');
+  const [context, setContext] = useState('');
+
   return (
     <main className="write">
       <section className="contents">
-        <header className="title">title</header>
-        <main className="context">context</main>
+        <section className="title">
+          <Label for="input_title">TITLE</Label>
+          <Input type="text" id="input_title" value={title} onChange={(e) => setTitle(e.target.value)} />
+        </section>
+        <section className="context">
+          <Label for="input_context">CONTEXT</Label>
+          <Input type="textarea" id="input_context" value={context} onChange={(e) => setContext(e.target.value)} />
+        </section>
       </section>
       <section className="submission">
         submission
